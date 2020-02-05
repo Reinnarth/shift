64247a9d3951b00359702e1b8c173c132ffe60a4
MalakorDark <malakordark@gmail.com>
2020-02-05 15:17:31 +0700

    timelist wip2

--- src/components/TimeList/index.js
+++ src/components/TimeList/index.js
@@ -4,7 +4,50 @@ export default function TimeList() {
 
     return (
         <div>
-            18:00
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Доступное время</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Тут будет форма и кнопка</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <tr>
+                        <td>9:00</td>
+                    </tr>
+                    <tr>
+                        <td>10:00</td>
+                    </tr>
+                    <tr>
+                        <td>11:00</td>
+                    </tr>
+                    <tr>
+                        <td>12:00</td>
+                    </tr>
+                    <tr>
+                        <td>13:00</td>
+                    </tr>
+                    <tr>
+                        <td>14:00</td>
+                    </tr>
+                    <tr>
+                        <td>15:00</td>
+                    </tr>
+                    <tr>
+                        <td>16:00</td>
+                    </tr>
+                    <tr>
+                        <td>17:00</td>
+                    </tr>
+                    <tr>
+                        <td>18:00</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
